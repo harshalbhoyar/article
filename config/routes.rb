34f26c66a7d8b89d605654b2229e5901e08e2a7f
Harshal Bhoyar <harshalbhoyar14@gmail.com>
2020-11-20 16:11:11 +0530

    Devise gem configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
-  get 'welcome/index'
+  devise_for :users
+  get 'books/index'
   resources :books
-  root 'welcome#index'
+  root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
